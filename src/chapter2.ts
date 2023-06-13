1fb67411fa002bd840c5f83a5f9318c7a50d8a99
longtime1116 <longtime1116@gmail.com>
2023-06-13 22:11:42 +0900

    chapter2 done

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -1,86 +1,84 @@
 namespace Chapter2 {
-  if (false) {
-    // プリミティブ
-    // 少数も整数も同じ number 型のリテラル
-    const width1 = 5;
-    const width2 = 8;
-    const height: number = 3;
-    const area = ((width1 + width2) * height) / 2;
-    console.log(area); // 19.5
+  // プリミティブ
+  // 少数も整数も同じ number 型のリテラル
+  const width1 = 5;
+  const width2 = 8;
+  const height: number = 3;
+  const area = ((width1 + width2) * height) / 2;
+  console.log(area); // 19.5
 
-    console.log(0b111111111); // 2進数
-    console.log(0o777); // 8進数
-    console.log(0xff); // 16進数
+  console.log(0b111111111); // 2進数
+  console.log(0o777); // 8進数
+  console.log(0xff); // 16進数
 
-    // 指数標記
-    console.log(1.2e4);
-    console.log(1e-3);
-    // 桁区切り
-    console.log(1_000_000);
-    // TypeScript における数値は、IEEE754倍精度浮動小数点数
-    // 2^53を超えると正確に表現できなくなる
-    console.log(0b11111111111111111111111111111111111111111111111111111); // 9007199254740991 ← 2^53-1
-    console.log(0b11111111111111111111111111111111111111111111111111111 + 1); // 9007199254740992
-    console.log(0b11111111111111111111111111111111111111111111111111111 + 2); // 9007199254740992 ← おかしくなる
-    // 小数点計算を正確にやりたければ、それ用の library を。整数ならBigIntを。
-    console.log(0.1 + 0.2); // 0.30000000000000004
+  // 指数標記
+  console.log(1.2e4);
+  console.log(1e-3);
+  // 桁区切り
+  console.log(1_000_000);
+  // TypeScript における数値は、IEEE754倍精度浮動小数点数
+  // 2^53を超えると正確に表現できなくなる
+  console.log(0b11111111111111111111111111111111111111111111111111111); // 9007199254740991 ← 2^53-1
+  console.log(0b11111111111111111111111111111111111111111111111111111 + 1); // 9007199254740992
+  console.log(0b11111111111111111111111111111111111111111111111111111 + 2); // 9007199254740992 ← おかしくなる
+  // 小数点計算を正確にやりたければ、それ用の library を。整数ならBigIntを。
+  console.log(0.1 + 0.2); // 0.30000000000000004
 
-    // Bigint型のリテラル。上限のない整数。
-    const bignum: bigint = 123n + 654n + 2n;
-    console.log(bignum);
-    console.log(5n / 2n); // 少数切り捨て
+  // Bigint型のリテラル。上限のない整数。
+  const bignum: bigint = 123n + 654n + 2n;
+  console.log(bignum);
+  console.log(5n / 2n); // 少数切り捨て
 
-    // 文字列リテラル
-    console.log(
-      "Hello\n\
+  // 文字列リテラル
+  console.log(
+    "Hello\n\
 World"
-    );
-    // テンプレートリテラル
-    console.log(
-      `Hello
+  );
+  // テンプレートリテラル
+  console.log(
+    `Hello
 World`
-    );
-    const helloStr: string = "hello";
-    console.log(`${helloStr}, World`); // 式を埋め込める
+  );
+  const helloStr: string = "hello";
+  console.log(`${helloStr}, World`); // 式を埋め込める
 
-    // 真偽値リテラル
-    const no: boolean = false;
-    const yes: boolean = true;
-    console.log(yes, no);
+  // 真偽値リテラル
+  const no: boolean = false;
+  const yes: boolean = true;
+  console.log(yes, no);
 
-    // nullとundefined
-    const val1: null = null;
-    const val2: undefined = undefined;
-    console.log(val1, val2);
+  // nullとundefined
+  const val1: null = null;
+  const val2: undefined = undefined;
+  console.log(val1, val2);
 
-    // 書籍にはないけど、シンボル。Rubyのそれとは違うので注意。基本使わないっぽい。
-    const tokenString: string = "tokenString";
-    const tokenString2: string = "tokenString";
-    const Sym = Symbol(tokenString);
-    const Sym2 = Symbol(tokenString2); // Sym2とSymは別物。
-    console.log(Sym.toString());
-    console.log(Symbol.for(tokenString));
-    console.log(Symbol.keyFor(Symbol.for(tokenString)));
+  // 書籍にはないけど、シンボル。Rubyのそれとは違うので注意。基本使わないっぽい。
+  const tokenString: string = "tokenString";
+  const tokenString2: string = "tokenString";
+  const Sym = Symbol(tokenString);
+  const Sym2 = Symbol(tokenString2); // Sym2とSymは別物。
+  console.log(Sym.toString());
+  console.log(Symbol.for(tokenString));
+  console.log(Symbol.keyFor(Symbol.for(tokenString)));
 
-    // 型変換
-    console.log(Number(true)); // 1
-    console.log(Number(false)); // 0
-    console.log(Number(null)); // 0
-    console.log(Number(undefined)); // NaN
-    console.log(BigInt("1234")); // 1234n
-    console.log(BigInt(500)); // 500n
-    console.log(BigInt(true)); // 1n
-    // console.log(BigInt("hoge")); // ランタイムエラー
-    console.log(String(true)); // true
-    console.log(Boolean(123)); // true
-    console.log(Boolean(0)); // false
-    console.log(Boolean(1n)); // true
-    console.log(Boolean(0n)); // false
-    console.log(Boolean("")); // false
-    console.log(Boolean("foobar")); // true
-    console.log(Boolean(null)); // false
-    console.log(Boolean(undefined)); // false
-  }
+  // 型変換
+  console.log(Number(true)); // 1
+  console.log(Number(false)); // 0
+  console.log(Number(null)); // 0
+  console.log(Number(undefined)); // NaN
+  console.log(BigInt("1234")); // 1234n
+  console.log(BigInt(500)); // 500n
+  console.log(BigInt(true)); // 1n
+  // console.log(BigInt("hoge")); // ランタイムエラー
+  console.log(String(true)); // true
+  console.log(Boolean(123)); // true
+  console.log(Boolean(0)); // false
+  console.log(Boolean(1n)); // true
+  console.log(Boolean(0n)); // false
+  console.log(Boolean("")); // false
+  console.log(Boolean("foobar")); // true
+  console.log(Boolean(null)); // false
+  console.log(Boolean(undefined)); // false
   let foo: number = 10;
   foo++;
   console.log(foo);
@@ -125,4 +123,68 @@ World`
   console.log(void n); // undefined
   const v: void = undefined;
   console.log(v);
+  // if文の条件式は、booleanに変換されて評価される
+  if ("") {
+    console.log("ここに入らない");
+  } else {
+    console.log("ここに入る");
+  }
+  if (0) {
+    console.log("ここに入らない");
+  } else {
+    console.log("ここに入る");
+  }
+  if (!!0) {
+    console.log("ここに入らない");
+  } else {
+    console.log("ここに入る");
+  }
+  console.log("hoge" === 10); // false
+  //const command: string = "greet";
+  const command: number = 10;
+  switch (command) {
+    case "greet": // 式が来る
+      console.log("こんにちは");
+      break;
+    case 5 + 5:
+      console.log("数値");
+      break;
+
+    case "greet":
+      console.log("こんにちは");
+      break;
+    case "cat":
+      console.log("猫");
+      break;
+    default:
+      console.log("デフォ");
+  }
+  // FizzBuzz
+  for (let i = 1; i < 16; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  let output: string = "";
+  for (let i = 1; i < 16; i++) {
+    if (i > 1) {
+      output += " ";
+    }
+    if (i % 15 === 0) {
+      output += "FizzBuzz";
+    } else if (i % 3 === 0) {
+      output += "Fizz";
+    } else if (i % 5 === 0) {
+      output += "Buzz";
+    } else {
+      output += String(i);
+    }
+  }
+  console.log(output);
 }
